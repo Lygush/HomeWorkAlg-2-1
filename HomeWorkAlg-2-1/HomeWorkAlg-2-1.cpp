66136d45558b2dbf243479afe97d3e18f23c465d
Ivan <kosarevsky2000@yandex.ru>
2023-07-18 21:19:35 +0300

    initial commit

---
+++ HomeWorkAlg-2-1/HomeWorkAlg-2-1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int Fibonacci(int n) {
+    if (n == 1 || n == 2) {
+        return 1;
+    }
+    return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+int main()
+{
+    int number{};
+    std::cin >> number;
+    std::cout << Fibonacci(number);
+}
+
+// Алгоритвическая сложность = О(2^n)
